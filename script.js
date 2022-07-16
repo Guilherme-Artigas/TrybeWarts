1f90921922000fb0a5188b0753c602fd898be48d
Guilherme Artigas <guilherme.artigas92@gmail.com>
2022-07-16 13:36:57 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -9,8 +9,10 @@ const fieldC = window.document.querySelector('#textarea');
 submitCheck.addEventListener('click', () => {
   if (submitCheck.checked === true) {
     submitBtn.removeAttribute('disabled');
+    submitBtn.classList.add('active');
   } else {
     submitBtn.setAttribute('disabled', '');
+    submitBtn.classList.remove('active');
   }
 });
 
@@ -23,7 +25,48 @@ btnLogin.addEventListener('click', (e) => {
   }
 });
 
-contadora.innerHTML = 500;
+contadora.innerHTML = `Restam ${500} caracteres.`;
 fieldC.addEventListener('keyup', () => {
-  contadora.innerHTML = 500 - fieldC.value.length;
+  contadora.innerHTML = `Restam ${500 - fieldC.value.length} caracteres.`;
+});
+
+submitBtn.addEventListener('click', () => {
+  const getConteudo = window.document.querySelectorAll('input.subject');
+  const listaConteudo = [];
+  for (let indice = 0; indice < getConteudo.length; indice += 1) {
+    if (getConteudo[indice].checked === true) {
+      listaConteudo.push(getConteudo[indice].value);
+    }
+  }
+  const cadastro = {
+    nome: window.document.querySelector('input#input-name').value,
+    sobrenome: window.document.querySelector('input#input-lastname').value,
+    email: window.document.querySelector('input#input-email').value,
+    house: window.document.querySelector('select#house').value,
+    family: window.document.querySelector('input[name="family"]:checked').value,
+    conteudo: listaConteudo,
+    avalicacao: window.document.querySelector('input[name="rate"]:checked').value,
+    coments: window.document.querySelector('textarea#textarea').value,
+  };
+  localStorage.setItem('cadastro', JSON.stringify(cadastro));
+});
+
+submitBtn.addEventListener('click', () => {
+  const mainFather = window.document.querySelector('main');
+  const formEvaluation = window.document.querySelector('form#evaluation-form');
+  const imgFormLogo = window.document.querySelector('img#trybewarts-forms-logo');
+  const cadastroString = localStorage.getItem('cadastro');
+  const objCadastro = JSON.parse(cadastroString);
+  formEvaluation.style.display = 'none';
+  mainFather.removeChild(imgFormLogo);
+  const newForm = window.document.createElement('form');
+  newForm.id = 'form-data';
+  mainFather.appendChild(newForm);
+  newForm.innerText = `Nome: ${objCadastro.nome} ${objCadastro.sobrenome}
+  Email: ${objCadastro.email}
+  Casa: ${objCadastro.house}
+  Família: ${objCadastro.family}
+  Matérias: ${objCadastro.conteudo[0]}, ${objCadastro.conteudo[1]}, ${objCadastro.conteudo[2]}
+  Avaliação: ${objCadastro.avalicacao}
+  Observações: ${objCadastro.coments}`;
 });
